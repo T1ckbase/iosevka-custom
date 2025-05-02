39c6476e0e2eadef5dc23bcc3d506488df53abba
T1ckbase <146760065+T1ckbase@users.noreply.github.com>
2025-05-03 02:10:14 +0800

    first commit

---
+++ main.ts
@@ -0,0 +1,10 @@
+import { Octokit } from '@octokit/rest';
+
+const octokit = new Octokit();
+
+const res = await octokit.repos.getLatestRelease({
+  owner: 'be5invis',
+  repo: 'Iosevka',
+});
+
+console.log(res.data.tag_name);
